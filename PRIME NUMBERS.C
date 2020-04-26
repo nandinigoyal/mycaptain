e975a5c5224c005c56a545f9136e5322ff19c30b
nandinigoyal <63860534+nandinigoyal@users.noreply.github.com>
2020-04-26 16:31:54 +0530

    PRIME NUMBER .C

---
+++ PRIME NUMBERS.C
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main()
+{
+	int n,i,count=0;
+	printf("Enter any positive number:");
+	scanf("%d",&n);
+	for (i=2;i<=n/2;i++)
+	{
+	if (n % i == 0) {
+    count = 1;
+   break;
+   }
+    }
+
+    if (n == 1) {
+    printf("1 is neither prime nor composite.");
+    }
+    else {
+   if (count== 0)
+   printf("%d is a prime number.", n);
+    else
+    printf("%d is not a prime number.", n);
+    }
+
+    return 0;
+}
+	
